02c149aa061f303dfbe8c8d79e28c6fc6c2253d4
John Bang <john.bang12@gmail.com>
2022-08-24 15:33:28 -0700

    adds metrics

---
+++ src/components/Metrics.js
@@ -0,0 +1,90 @@
+import React from "react";
+import {
+  Table,
+  Col,
+  Button,
+  ToggleButtonGroup,
+  ToggleButton,
+} from "react-bootstrap";
+import { XCircle } from "react-bootstrap-icons";
+
+function Metrics(props) {
+  // eslint-disable-next-line no-unused-vars
+  const { metrics, metricsToShow, setMetricsToShow } = props;
+
+  const handleToggleMetricToAdd = (id, metric) => {
+    if (id.target.value === 1) {
+      setMetricsToShow((metricsToShow) => [...metricsToShow, metric]);
+      // set elemnt to active
+    }
+    if (id.target.value === 2) {
+      setMetricsToShow((metricsToShow) =>
+        metricsToShow.filter((m) => m.id !== metric.id)
+      );
+      // set elemnt to inactive
+    }
+  };
+
+  const handleDefaultMetricButtonValue = (metric) => {
+    const res = metricsToShow.filter((m) => m.id === metric.id)[0] ? 1 : 2;
+    console.log(res);
+    return res;
+  };
+  // const setDefaultMetricButtonActive = (metric) => {
+  // 	const res =  metricsToShow.filter(m => m.id === metric.id)[0] ? 1 : 2;
+  // 	console.log(res);
+  // 	return res;
+  // };
+
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Saved Metrics</th>
+          <th>Add Metric to Class?</th>
+          <th className="text-center">Remove</th>
+        </tr>
+      </thead>
+      <tbody>
+        {metrics &&
+          metrics.map((metric, idx) => {
+            // setDefaultMetricButtonActive(metric);
+            return (
+              <tr key={idx}>
+                <td>{metric.name}</td>
+                <td>
+                  <ToggleButtonGroup
+                    type="radio"
+                    name="options"
+                    defaultValue={handleDefaultMetricButtonValue(metric)}
+                    onChange={(e) => handleToggleMetricToAdd(e, metric)}
+                  >
+                    <ToggleButton id={1} variant="outline-dark" value={1}>
+                      Yes
+                    </ToggleButton>
+                    <ToggleButton id={2} variant="outline-dark" value={2}>
+                      No
+                    </ToggleButton>
+                  </ToggleButtonGroup>
+                </td>
+                <td>
+                  <Col className="center-vertically center-horizontally">
+                    <Button
+                      variant="outline-dark"
+                      onClick={() => {
+                        console.log("delet");
+                      }}
+                    >
+                      <XCircle size={25} />
+                    </Button>
+                  </Col>
+                </td>
+              </tr>
+            );
+          })}
+      </tbody>
+    </Table>
+  );
+}
+
+export default Metrics;
